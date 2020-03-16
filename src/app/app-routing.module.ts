c407e9bfbee77b95352118e471f701cbfd4261c3
Jose Daniel <jose_daniel@outlook.com>
2020-03-15 22:32:36 -0500

    fin seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ItemComponent } from './pages/item/item.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: PortafolioComponent
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent
+  },
+  {
+    path: 'portafolio',
+    component: PortafolioComponent
+  },
+  {
+    path: 'item',
+    component: ItemComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'inicio'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
